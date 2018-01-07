3fdff411ed09dc712b61c90c182b012a4ed9ece8
Oscar Wos <theoscar@protonmail.com>
2018-01-07 21:51:15 +0000

    Revert "Merge from OLD REPO"
    This reverts commit 1c67e30a5c2f1459bd7bbf631eaeb77c69d9eb97.

--- Compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIRECTORY="/home/oscar/Git/AdminUtilChat"
-FILENAME="Admin-Util-Chat"
-
-~/Git/SourceModScripting/spcomp $DIRECTORY/scripting/$FILENAME.sp -o$DIRECTORY/plugins/$FILENAME.smx
--- Upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIRECTORY="/home/oscar/Git/AdminUtilChat"
-
-git -C $DIRECTORY add --all
-git -C $DIRECTORY commit -a
-git -C $DIRECTORY push
